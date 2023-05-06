ae9523421a9fb9d74a4938f8c477da372223a9a8
Martin Rooden <mrooden@tktk.ee>
2023-05-06 15:48:24 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See on lihtsalt kommentaar- ei osale rakenduse töös
+
+// Rakendus küsib kasutalt nime- seejärel rakendus tervitab nimepidi
+
+Console.WriteLine("Enter Your Name");// output
+// string on sõne-andmetüüp mis läheb kasti sisse
+string userName = Console.ReadLine();// Input
+stri
+
+Console.WriteLine("Hello"+", "+ userName+"!");// Output
+// Tänapäevasem on kasutada string interpolation- palju mugava ja kiirem kui ülalolev meetod.
+
+Console.WriteLine($"Hello, {userName} !");//Output
